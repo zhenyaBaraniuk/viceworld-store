4dc432cdd459756fcdf1087e7ba38ae0721b0259
Zhenya Baraniuk <y.baraniuk@ideil.com>
2026-05-06 14:21:23 +0300

    Add catalog page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Error\NotFoundController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-
-Route::get('/', fn() => Inertia::render('Home/index'));
+Route::get('/catalog', fn() => Inertia::render('Catalog/index'));
 Route::get('/404', NotFoundController::class);
 Route::get('/500', fn() => Inertia::render('Error/ServerError'));
+Route::get('/', fn() => Inertia::render('Home/index'));
