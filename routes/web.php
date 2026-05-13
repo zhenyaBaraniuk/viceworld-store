7ba43caba4273aa7f8db71242b79553189ea054a
Zhenya Baraniuk <zhenyabaraniuk@gmail.com>
2026-05-13 23:29:55 +0300

    Add account orders page

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Inertia\Inertia;
 
 Route::get('/catalog', fn() => Inertia::render('Catalog/index'));
 Route::get('/search', fn() => Inertia::render('Search/index'));
+Route::get('/profile', fn() => Inertia::render('Profile/index'));
 Route::get('/product', fn() => Inertia::render('Product/index'));
 Route::get('/404', NotFoundController::class);
 Route::get('/500', fn() => Inertia::render('Error/ServerError'));
