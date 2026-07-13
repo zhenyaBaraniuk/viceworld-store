c88aeb3f35ed08856aca5f31b717358de29419aa
Zhenya Baraniuk <zhenyabaraniuk@gmail.com>
2026-07-13 12:37:43 +0300

    fix: Prevent full page reload using persistent layout

--- resources/js/Pages/Product/index.tsx
+++ resources/js/Pages/Product/index.tsx
@@ -1,37 +1,33 @@
-import Header from "../../Components/Header";
-import Footer from "../../Components/Footer";
 import AdditionalProducts from "./AdditionalProducts";
 import Breadcrumbs from "../../Navigation/Breadcrumbs";
 import Product from "./Product";
 import { route } from "@/lib/route";
 import type { ProductProps } from "@/types";
+import SiteLayout from "@/Layouts/SiteLayout";
+import { ReactNode } from "react";
 
-export default function ProductPage({
-    product,
-    category,
-    related_products,
-}: ProductProps) {
+function ProductPage({ product, category, related_products }: ProductProps) {
     return (
-        <>
-            <Header />
-            <main className="max-w-[1440px] mx-auto px-6 md:px-12 pb-24 pt-20">
-                <Breadcrumbs
-                    items={[
-                        { label: "Home", href: "/" },
-                        {
-                            label: category.name,
-                            href: route("catalog.show", {
-                                slug: category.slug,
-                            }),
-                        },
-                        { label: product.name },
-                    ]}
-                />
+        <div className="max-w-[1440px] mx-auto px-6 md:px-12 pb-24 pt-20">
+            <Breadcrumbs
+                items={[
+                    { label: "Home", href: "/" },
+                    {
+                        label: category.name,
+                        href: route("catalog.show", {
+                            slug: category.slug,
+                        }),
+                    },
+                    { label: product.name },
+                ]}
+            />
 
-                <Product product={product} />
-                <AdditionalProducts related_products={related_products} />
-            </main>
-            <Footer />
-        </>
+            <Product product={product} />
+            <AdditionalProducts related_products={related_products} />
+        </div>
     );
 }
+
+ProductPage.layout = (page: ReactNode) => <SiteLayout>{page}</SiteLayout>;
+
+export default ProductPage;
--- resources/js/Pages/Search/index.tsx
+++ resources/js/Pages/Search/index.tsx
@@ -1,11 +1,11 @@
-import Header from "../../Components/Header";
 import SearchHeader from "../Search/SearchHeader";
 import Filters from "../Search/Filters";
 import Products from "../Search/Products";
-import Footer from "../../Components/Footer";
 import type { SearchProps } from "@/types";
+import SiteLayout from "@/Layouts/SiteLayout";
+import { ReactNode } from "react";
 
-export default function Search({
+function Search({
     products,
     categories,
     filters,
@@ -15,26 +15,22 @@ export default function Search({
     total,
 }: SearchProps) {
     return (
-        <>
-            <Header />
-            <main className="max-w-[1440px] mx-auto px-6 md:px-12 pb-24">
-                <SearchHeader query={query} total={total} />
-                <div className="flex flex-col lg:flex-row gap-16">
-                    <Filters
-                        categories={categories}
-                        filters={filters}
-                        max_price={max_price}
-                        colors={colors}
-                        query={query}
-                    />
-                    <Products
-                        products={products}
-                        filters={filters}
-                        query={query}
-                    />
-                </div>
-            </main>
-            <Footer />
-        </>
+        <div className="max-w-[1440px] mx-auto px-6 md:px-12 pb-24">
+            <SearchHeader query={query} total={total} />
+            <div className="flex flex-col lg:flex-row gap-16">
+                <Filters
+                    categories={categories}
+                    filters={filters}
+                    max_price={max_price}
+                    colors={colors}
+                    query={query}
+                />
+                <Products products={products} filters={filters} query={query} />
+            </div>
+        </div>
     );
 }
+
+Search.layout = (page: ReactNode) => <SiteLayout>{page}</SiteLayout>;
+
+export default Search;
